45f7c3e165127f3e66e0c7de8fcc2b1c3c4a4092
samiamarante <135390658+samiamarante@users.noreply.github.com>
2025-05-24 12:46:04 +0000

    Completed exercises

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -2,38 +2,69 @@
 -- Complete each mission by writing your SQL query below the instructions.
 -- Don't forget to end each query with a semicolon ;
 
-SELECT * FROM regions;
-SELECT * FROM species;
-SELECT * FROM climate;
-SELECT * FROM observations;
+SELECT
+    reg.name AS region_name,
+    reg.country,
+    COUNT(DISTINCT obs.species_id) AS total_species
+FROM regions reg
+JOIN observations obs ON reg.id = obs.region_id
+GROUP BY reg.id, reg.name, reg.country
+ORDER BY total_species DESC;
 
+SELECT
+    strftime('%m', observation_date) AS mes,
+    COUNT(observation_date) cantidad_observación
+FROM observations
+GROUP BY mes
+ORDER BY cantidad_observación DESC;
 
--- MISSION 1
--- Your query here
+SELECT
+    obs.species_id,
+    sp.scientific_name,
+    SUM(obs.count) AS total_individuos
+FROM observations obs
+JOIN species sp ON obs.species_id = sp.id
+GROUP BY obs.species_id, sp.scientific_name
+HAVING total_individuos < 5
+ORDER BY total_individuos ASC;
 
--- MISSION 2
--- Your query here:
+SELECT
+    reg.name AS region_name,
+    reg.country,
+        COUNT(DISTINCT obs.species_id) AS total_species
+FROM regions reg
+JOIN observations obs ON reg.id = obs.region_id
+GROUP BY reg.id, reg.name, reg.country
+ORDER BY total_species DESC
+LIMIT 1;
 
+SELECT 
+    obs.species_id,
+    sp.common_name,
+        SUM(obs.count) AS total_observación
+    FROM observations obs
+JOIN species sp ON obs.species_id = sp.id
+GROUP BY obs.species_id, sp.common_name
+ORDER BY total_observación DESC;
 
--- MISSION 3
--- Your query here:
+SELECT
+    observer,
+    COUNT(*) AS total_registros
+FROM observations
+GROUP BY observer
+ORDER BY total_registros DESC;
 
+SELECT
+    sp.id,
+    sp.common_name,
+    sp.scientific_name
+FROM species sp
+LEFT JOIN observations obs ON sp.id = obs.species_id
+WHERE obs.species_id IS NULL;
 
--- MISSION 4
--- Your query here:
-
-
--- MISSION 5
--- Your query here:
-
-
--- MISSION 6
--- Your query here:
-
-
--- MISSION 7
--- Your query here:
-
-
--- MISSION 8
--- Your query here:
+SELECT
+    observation_date,
+    COUNT(DISTINCT species_id) AS especies_distintas
+FROM observations
+GROUP BY observation_date
+ORDER BY especies_distintas DESC;
